1092c9007ff7a1c9592ade9ae40ea69e2c89fa6f
Nishchal Kush <kush@kush.com.fj>
2019-05-18 15:22:43 +1000

    added command line install for chocolatey, added common sysadmin and analysis tools

--- win-setup-choco.cmd
+++ win-setup-choco.cmd
@@ -4,8 +4,10 @@ rem Assumes that chocolatey is already installed, else use the following Powersh
 rem Set-ExecutionPolicy Bypass -Scope Process -Force; 
 rem   `iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
 rem Upgrade chocolatey
-choco upgrade chocolatey
+rem #choco upgrade chocolatey
 
 rem Install Oracle Virtualbox 
 choco install virtualbox --yes
@@ -15,12 +17,14 @@ choco install firefox --yes
 choco install googlechrome --yes
 choco install nordvpn --yes
 
-rem Install Network traffic capture tools
-choco install wireshark --yes
+rem Install Windows management and admin tools
+choco install rsat --yes
+choco install sysinternals --yes
 
 rem Install Utilities
 choco install 7zip --yes
-choco install sysinternals --yes
+choco install bind-toolsonly --yes
+choco install cygwin --yes
 choco install putty --yes
 
 rem Install password manager
@@ -30,19 +34,34 @@ rem Install Dropbox
 choco install dropbox --yes
 
 rem Install useful applications
-choco install neovim --yes
+choco install vim --yes
 choco install vlc --yes
 choco install pdfxchangeeditor --yes
 
-rem Install Arduino development environment
-choco install arduino --yes
+rem Install Network traffic capture and analysis tools
+choco install microsoft-message-analyzer --yes
+choco install winpcap --yes
+choco install wireshark --yes
 
-rem Install Git tools
+rem Install web traffic analysis tools
+choco install fiddler --yes
+choco install burp-suite-free-edition --yes
+
+rem Install memory analysis tools
+choco install vilatility --yes
+
+rem Install development tools
+choco install python --yes
+choco install arduino --yes
 choco install git --yes
 choco install github --yes
 
+rem Install communications tools and utilities
+choco install slack --yes
+choco install signal --yes
+
 rem Upgrade all other installed applications
-choco upgrade all
+rem #choco upgrade all
 
 rem List all installed applications
 choco list --local-only
