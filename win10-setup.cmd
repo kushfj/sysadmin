3f80a141565df20e8a98647d33282b114d42be6c
kushfj <kush@kush.com.fj>
2019-07-29 11:39:55 +1000

    initial version

---
+++ win10-setup.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+rem must be run with elevated privileges
+
+rem disable windows 10 telemetry
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection /v AllowTelemetry /t REG_DWORD /d 0
