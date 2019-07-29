aa47dae6cb05d692e3ac2e7634b727559912ba69
kushfj <kush@kush.com.fj>
2019-07-29 16:10:02 +1000

    added commands to reset firewall to windows default, and disable telemetry and diagnostics

--- win10-setup.cmd
+++ win10-setup.cmd
@@ -4,3 +4,8 @@ rem must be run with elevated privileges
 
 rem disable windows 10 telemetry
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection /v AllowTelemetry /t REG_DWORD /d 0
+sc config diagtrack start= disabled
+sc stop diagtrack
+
+rem set windows firewall to defaults
+netsh advfirewall reset
