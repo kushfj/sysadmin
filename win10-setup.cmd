94e06be3b38c429b890e0e1e0d3e4e6b3896f5e9
kushfj <kush@kush.com.fj>
2019-08-25 21:52:16 +1000

    added loop to disable redundant windows services

--- win10-setup.cmd
+++ win10-setup.cmd
@@ -6,8 +6,8 @@ rem TODO: add checking for elevated privileges and attempt elevation via UAC
 rem disable windows 10 telemetry
 echo "[+] disabling windows10 telemetry"
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection /v AllowTelemetry /t REG_DWORD /d 0 /f
-sc config diagtrack start= disabled
-sc stop diagtrack
+sc stop "DiagTrack"
+sc config "DiagTrack" start=disabled
 
 rem disable active probing for internet connection
 echo "[+] disabling active internet connection probing"
@@ -21,11 +21,6 @@ rem disbale netbios over TCP/IP on all interfaces
 echo "[+] disabling NetBIOS over TCP on all interfaces"
 for /f %%i in ('reg query HKLM\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\') do reg add "%%i" /v "NetbiosOptions" /t REG_DWORD /d 2 /f
 
-rem stop and disable simple service discovery protocol (SSDP)
-echo "[+] disabling simple service discovery protocol"
-sc stop "SSDPSRV" 
-sc config "SSDPSRV" start=disabled
-
 rem disable multicast internet group manangement protocol (IGMP)
 echo "[+] disabling internet group management protocol"
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "IGMPLevel" /t REG_DWORD /d 0 /f 
@@ -37,6 +32,40 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\tcpip6\Parameters" /v "DisabledC
 rem disable universal plug and play/pray (UPnP)
 echo "[+] disabling UPnP"
 reg add "HKLM\Software\Microsoft\DirectplayNATHelp\DPNHUPnP" /v "UPnPMode" /t REG_DWORD /d 2 /f
+
+rem disable redundant services
+for %%j in (
+  "diagnosticshub.standardcollector.service" 
+  "dmwappushservice"
+  "HomeGroupListener"
+  "HomeGroupProvider"
+  "lfsvc"
+  "MapsBroker"
+  "NetTcpPortSharing"
+  "RemoteAccess"
+  "RemoteRegistry"
+  "SharedAccess"
+  "TrkWks"
+  "WbioSrvc"
+  "WMPNetworkSvc"
+  "XblAuthManager"
+  "XblGameSave"
+  "XboxNetApiSvc"
+  "ndu"
+) do (
+echo "[+] disabling %%j"
+sc stop "%%j" 
+sc config "%%j" start=disabled
+)
+
+rem stop and disable simple service discovery protocol (SSDP)
+echo "[+] disabling simple service discovery protocol"
+sc stop "SSDPSRV" 
+sc config "SSDPSRV" start=disabled
+
+rem stop and disable 
+sc stop "WinHttpAutoProxySvc"
+sc config "WinHttpAutoProxySvc" start=disabled
 
 rem set windows firewall to defaults
 echo "[+] resetting windows firewall"
