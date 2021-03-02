8be48cc922a9ddb781ce415de01f4f7298d0d758
kushfj <kush@kush.com.fj>
2021-03-02 15:43:08 +1000

    added additional debug output for start and end time of main script loop (does not sync with threads), added start /b to run actual netsh in background as separate threads, NOTE *** this is not an elegant threading solution and can potentially consume 100% of CPU ***

--- smb-block.cmd
+++ smb-block.cmd
@@ -1,5 +1,7 @@
 @echo off
 echo [*] start blocking SMB and NetBIOS script
+set starttime=%time%
+echo [+] start time
 
 rem windows batch script to create firewall rules to block SMB and NetBIOS to
 rem Internet sites from local host. The following traffic is blocked
@@ -25,12 +27,18 @@ echo [+] getting ip addresses
 for /f %%f in (internet.txt) do (
 	set /a count+=1
 	echo [+] blocking SMB/NetBIOS access to %%f
-	netsh advfirewall firewall add rule name="Block SMB/NBSS to Internet - %%f" dir=out action=block enable=yes Profile=any Localip=any Remoteip=%%f Protocol=tcp Interfacetype=any remoteport=445,139
-	netsh advfirewall firewall add rule name="Block NBDS/NBNR to Internet - %%f" dir=out action=block enable=yes Profile=any Localip=any Remoteip=%%f Protocol=udp Interfacetype=any remoteport=137,138
+	start /b netsh advfirewall firewall add rule name="Block SMB/NBSS to Internet - %%f" dir=out action=block enable=yes Profile=any Localip=any Remoteip=%%f Protocol=tcp Interfacetype=any remoteport=445,139
+	start /b netsh advfirewall firewall add rule name="Block NBDS/NBNR to Internet - %%f" dir=out action=block enable=yes Profile=any Localip=any Remoteip=%%f Protocol=udp Interfacetype=any remoteport=137,138
 )
 echo [+] read %count% IPs
 
 
 endlocal
 echo [*] done
+set endtime=%time%
+echo [-] start: %starttime%
+echo [-] end: %endtime%
 pause
+
+rem cleanup all outgoing rules
+rem netsh advfirewall firewall delete rule name=all dir=out
